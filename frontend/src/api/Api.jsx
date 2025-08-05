cbdc243f1bd87750145ea001ae530787e07344a2
arvind1602 <arvindnagpure72230@gmail.com>
2025-08-05 16:51:00 +0530

    add dist

--- frontend/src/api/Api.jsx
+++ frontend/src/api/Api.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://mini-linkedin-backend-5ty8.onrender.com" });
+const API = axios.create({
+  baseURL: "https://mini-linkedin-backend-5ty8.onrender.com",
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
