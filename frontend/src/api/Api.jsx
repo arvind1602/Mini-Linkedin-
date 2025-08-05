e8cd4b798ce54a97f28d4caab75e231b5c38705f
arvind1602 <arvindnagpure72230@gmail.com>
2025-08-05 16:40:34 +0530

    change url

--- frontend/src/api/Api.jsx
+++ frontend/src/api/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8000/api" });
+const API = axios.create({ baseURL: "https://mini-linkedin-backend-5ty8.onrender.com" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
