de1b775a7c39a553d73c239bddfcddf58cb5d688
arvind1602 <arvindnagpure72230@gmail.com>
2025-08-05 16:57:53 +0530

    add cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,23 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
+//router import
+import userrouter from "./routers/User.route.js";
+import postrouter from "./routers/Post.route.js";
 
-
-//router import 
-import userrouter from "./routers/User.route.js"
-import postrouter from "./routers/Post.route.js"
-
-app.use("/api/users",userrouter);
-app.use("/api/posts" , postrouter)
+app.use("/api/users", userrouter);
+app.use("/api/posts", postrouter);
 
 export default app;
